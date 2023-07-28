710365aea146d7e90379a6e82087dc8f8e15f04e
Jan Betley <jan.betley@gmail.com>
2023-07-28 17:40:24 +0200

    use_model update

--- use_model.py
+++ use_model.py
@@ -5,10 +5,10 @@ import torch as t
 from models import SimpleModel
 from utils import pbn_to_repr, repr_to_pbn
 
-model = SimpleModel(2)
-model.load_state_dict(t.load("model_2_500_20.pth"))
+model = SimpleModel()
+model.load_state_dict(t.load("model_2_300_299.pth"))
 # %%
-pbn = '2.2.. KQ... JT... 98...'
+pbn = '2.6.. A.5.. ...34 98...'
 in_ = pbn_to_repr(pbn)
 out = model(in_.unsqueeze(0))[0].round()
 out_pbn = repr_to_pbn(out)
