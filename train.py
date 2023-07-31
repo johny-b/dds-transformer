27f6522007dc51d6846caf7132d1ff2d6e2b3fe6
Jan Betley <jan.betley@gmail.com>
2023-07-31 12:05:01 +0200

    first transformer, learns some stuff

--- train.py
+++ train.py
@@ -12,7 +12,7 @@ import torch as t
 import torch.nn.functional as F
 from torch.utils.data import DataLoader
 
-from models import SimpleModel
+from models import SimpleModel, TransformerModel
 from datasets import Dataset
 
 device = "cuda"
@@ -20,21 +20,66 @@ writer = SummaryWriter('runs')
 
 # %%
 
-trainset = Dataset({
-    2: list(range(10)),
-})
-testset = Dataset({
-    2: [99],
-})
+trainset = Dataset(
+    {
+        2: list(range(10)),
+    },
+)
+testset = Dataset(
+    {
+        2: [99],
+    },
+)
 
 # %%
-model = SimpleModel().to(device)
+from torch import nn
+  
 
-batch_size = 256
-epochs = 10
+class TransformerModel(nn.Module):
+    def __init__(self, d_model=24, nhead=12):
+        super().__init__()
+        self.embed = nn.Linear(52, d_model)
+        self.enc_1 = nn.TransformerEncoderLayer(d_model=d_model, nhead=nhead)
+        self.enc_2 = nn.TransformerEncoderLayer(d_model=d_model, nhead=nhead)
+        self.enc_3 = nn.TransformerEncoderLayer(d_model=d_model, nhead=nhead)
+        self.enc_4 = nn.TransformerEncoderLayer(d_model=d_model, nhead=nhead)
+        self.unembed = nn.Linear(4 * d_model, 208)
+        self.softmax = nn.Softmax(dim=1)
+    
+    def forward(self, x: t.Tensor):
+        assert len(x.shape) == 2
+        assert x.shape[1] == 208
+        
+        x = x.reshape((x.shape[0], 4, 52))
+
+        num_cards = x.flatten(start_dim=1).sum(dim=1)
+        expected_num_cards = (num_cards - 1).unsqueeze(1)
+        
+        x = t.stack((
+            self.embed(x[:,0,:]),
+            self.embed(x[:,1,:]),
+            self.embed(x[:,2,:]),
+            self.embed(x[:,3,:]),
+        )).permute((1,0,2))
+        
+        x = self.enc_1(x)
+        x = self.enc_2(x)
+        x = self.enc_3(x)
+        x = self.enc_4(x)
+        x = x.flatten(start_dim=1)
+        x = self.unembed(x)
+        x = self.softmax(x)
+        x = x * expected_num_cards
+
+        return x
+        
+model = TransformerModel().to(device)
+
+batch_size = 128
+epochs = 100
 
 train_loader = DataLoader(trainset, batch_size=batch_size, shuffle=True)
-test_loader = DataLoader(testset, batch_size=10000, shuffle=True)
+test_loader = DataLoader(testset, batch_size=batch_size, shuffle=True)
 
 optimizer = t.optim.AdamW(model.parameters(), weight_decay=0.02, lr=0.0002)
 train_loss_list = []
@@ -56,7 +101,7 @@ def get_loss_and_acc(model, inputs, labels):
     
 	
 for epoch_ix, epoch in enumerate(tqdm(range(epochs))):
-    for inputs, labels in train_loader:
+    for i, (inputs, labels) in enumerate(train_loader):
         loss, acc = get_loss_and_acc(model, inputs, labels)
         train_loss_list.append(loss.item())
         train_accuracy.append(acc)
@@ -83,7 +128,8 @@ for epoch_ix, epoch in enumerate(tqdm(range(epochs))):
         epoch_ix,
     )
     
-    
+# %%
+t.save(model.state_dict(), "transformer.pth")
 
 # %%
 
